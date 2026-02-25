b1f84eb93183fb625637f62478e6e78030b35113
DaniVagyok482 <gargyadaniel6@gmail.com>
2026-02-25 11:05:58 +0100

    Add files via upload

---
+++ register.js
@@ -0,0 +1,11 @@
+function reg(){
+    const email = document.getElementById("email").value
+    const passwd = document.getElementById("passwd1").value
+    const passwd2 = document.getElementById("passwd2").value
+    if(passwd === passwd2 && passwd.length > 5 && email.includes("@")){
+        
+    }else{
+        
+    }
+}
+
