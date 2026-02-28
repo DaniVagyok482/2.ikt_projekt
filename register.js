93be44c76ac465be2bbddeb3df9d1c573943e1d1
LipcseSzabolcs <lipcseiszabolcs3@gmail.com>
2026-02-28 11:26:36 +0000

    hozzá adtam a szemet
    félóra szenvedés de megvan

--- register.js
+++ register.js
@@ -8,4 +8,15 @@ function reg(){
         
     }
 }
+/*szem müködése*/
+function togglePassword(inputId, icon){
+    const pass = document.getElementById(inputId);
 
+    if(pass.type === "password"){
+        pass.type = "text";
+        icon.classList.replace("fa-eye","fa-eye-slash");
+    }else{
+        pass.type = "password";
+        icon.classList.replace("fa-eye-slash","fa-eye");
+    }
+}
