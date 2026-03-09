f4441083fd104beeb57f123c166dabf8db247e87
LipcseSzabolcs <lipcseiszabolcs3@gmail.com>
2026-03-09 09:54:07 +0000

    Add files via upload

--- register.js
+++ register.js
@@ -3,9 +3,14 @@ function reg(){
     const passwd = document.getElementById("passwd1").value
     const passwd2 = document.getElementById("passwd2").value
     if(passwd === passwd2 && passwd.length > 5 && email.includes("@")){
-        
+
+        localStorage.setItem("email", email)
+        localStorage.setItem("password", passwd)
+
+        window.location.href = "jegyzet.html";
+
     }else{
-        
+        alert("Valami baj van a registrácioval!!")
     }
 }
 /*szem müködése*/
@@ -19,4 +24,21 @@ function togglePassword(inputId, icon){
         pass.type = "password";
         icon.classList.replace("fa-eye-slash","fa-eye");
     }
+}
+/*bejelentkezés */
+function bejelentkezés(){
+    
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("passwd1").value;
+
+    let storedEmail = localStorage.getItem("email");
+    let storedPassword = localStorage.getItem("password");
+
+    if(email === storedEmail && password === storedPassword){
+        window.location.href = "jegyzet.html";
+    }
+    else{
+        alert("A felhasználó név vagy a jelszó hibás!")
+    }
+
 }
